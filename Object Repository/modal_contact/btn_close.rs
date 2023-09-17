ff6a8ac6d5589c5fbb172dd98e39b9915110dc50
sariwahyunita <wahyunitas@gmail.com>
2023-09-17 14:55:16 +0700

    implement ddt on Contact Module

---
+++ Object Repository/modal_contact/btn_close.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_close</name>
+   <tag></tag>
+   <elementGuidId>35f16719-2180-4e9d-8d72-f54a035b33de</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[.='Close']//following::button[.='Log in']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
