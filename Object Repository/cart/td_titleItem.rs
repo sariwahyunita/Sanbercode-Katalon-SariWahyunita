7114fb95f9471cdf589bd5b1a14075c0ee903b8a
sariwahyunita <wahyunitas@gmail.com>
2023-09-17 19:11:17 +0700

    add tc module home : - filter categories - view detail - add to card

---
+++ Object Repository/cart/td_titleItem.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_titleItem</name>
+   <tag></tag>
+   <elementGuidId>d28c20a4-84d3-4608-8da0-5e4c14af7e42</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//tr[@class='success']/td[contains(text(),'Samsung galaxy s6')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/home/sidebar/a_categories.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_categories</name>
+   <tag></tag>
+   <elementGuidId>e6d59f6e-156e-485b-8d05-70821c0f934a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='cat'][.='CATEGORIES']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
